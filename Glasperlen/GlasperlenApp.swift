997a170c4e93b38b250567d805ec691328cfb7c3
vauxhall <zero.griffin@gmail.com>
2020-10-28 09:58:46 +0100

    Initial Commit

---
+++ Glasperlen/GlasperlenApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlasperlenApp.swift
+//  Glasperlen
+//
+//  Created by vaux on 28.10.20.
+//
+
+import SwiftUI
+
+@main
+struct GlasperlenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
