8a4f4607214fd9816e4d78636ffd864e6f73e0ee
vauxhall <zero.griffin@gmail.com>
2020-11-09 16:05:10 +0100

    Update shadows

--- Neumorphic.swift
+++ Neumorphic.swift
@@ -4,6 +4,6 @@ struct Neumorphic: ViewModifier {
     func body(content: Content) -> some View {
         content
             .shadow(color: Color.black.opacity(0.7), radius: 12, x: 12, y: 12)
-            .shadow(color: Color.white.opacity(0.06), radius: 5, x: -5, y: -5)
+            .shadow(color: Color.white.opacity(0.05), radius: 5, x: -5, y: -5)
     }
 }
