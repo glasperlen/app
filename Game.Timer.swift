e63c9b1b03499a72697a301e792d091108955b8e
vaux <zero.griffin@gmail.com>
2020-12-02 10:55:17 +0100

    Add timer

--- Game.Timer.swift
+++ Game.Timer.swift
@@ -5,6 +5,8 @@ extension Game {
     struct Timer: View {
         @Binding var session: Session
         let wait: Match.Wait
+        @State private var date = ""
+        private let timer = SwiftUI.Timer.publish(every: 1, on: .main, in: .common).autoconnect()
         
         var body: some View {
             if session.match?[wait.player.negative].id.isEmpty == false {
@@ -13,9 +15,14 @@ extension Game {
                         Text("Waiting")
                             .font(.caption2)
                     }
-                    Text(wait.timeout, style: .relative)
-                        .font(.footnote)
+                    Text(verbatim: date)
+                        .font(Font.footnote.monospacedDigit())
                         .foregroundColor(.secondary)
+                }.onReceive(timer) {
+                    let formatter = DateComponentsFormatter()
+                    formatter.allowedUnits = [.second]
+                    formatter.unitsStyle = .short
+                    date = formatter.string(from: $0, to: wait.timeout)!
                 }
             }
         }
