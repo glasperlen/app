842ac4833e074956399a0bd99690d193b582a590
vaux <zero.griffin@gmail.com>
2020-12-08 14:07:31 +0100

    Update new game

--- Game.Controls.swift
+++ Game.Controls.swift
@@ -3,6 +3,7 @@ import SwiftUI
 extension Game {
     struct Controls: View {
         @Binding var session: Session
+        @Binding var new: Bool
         @State private var menu = false
         @State private var inventory = false
         @State private var alert = false
@@ -17,28 +18,41 @@ extension Game {
                     Inventory(session: $session)
                 }
                 Control.Circle(image: "plus") {
-                    alert = true
-                }
-                .actionSheet(isPresented: $alert) {
                     if session.beads.filter(\.selected).count == 5 {
-                        return .init(title: .init("New game"),
-                                     buttons: [
-                                        .default(.init("Single")) {
-                                            session.match = .init()
-                                        },
-                                        .default(.init("Multiplayer")) {
-                                            UIApplication.shared.request()
-                                        },
-                                        .cancel()])
+                        withAnimation(.easeInOut(duration: 0.35)) {
+                            new = true
+                        }
                     } else {
-                        return .init(title: .init("Need 5 beads selected to play"),
-                                     message: .init("Select them in your inventory"),
-                                     buttons: [
-                                         .default(.init("Inventory")) { inventory = true },
-                                         .cancel()])
+                        alert = true
                     }
                 }
-                Control.Circle(image: "line.horizontal.3") {
+                .actionSheet(isPresented: $alert) {
+                    .init(title: .init("Need 5 beads selected to play"),
+                                 message: .init("Select them in your inventory"),
+                                 buttons: [
+                                     .default(.init("Inventory")) { inventory = true },
+                                     .cancel()])
+                }
+//                .actionSheet(isPresented: $alert) {
+//                    if session.beads.filter(\.selected).count == 5 {
+//                        return .init(title: .init("New game"),
+//                                     buttons: [
+//                                        .default(.init("Single")) {
+//                                            session.match = .init()
+//                                        },
+//                                        .default(.init("Multiplayer")) {
+//                                            UIApplication.shared.request()
+//                                        },
+//                                        .cancel()])
+//                    } else {
+//                        return .init(title: .init("Need 5 beads selected to play"),
+//                                     message: .init("Select them in your inventory"),
+//                                     buttons: [
+//                                         .default(.init("Inventory")) { inventory = true },
+//                                         .cancel()])
+//                    }
+//                }
+                Control.Circle(image: "slider.vertical.3") {
                     menu = true
                 }
                 .sheet(isPresented: $menu) {
--- Game.Title.swift
+++ Game.Title.swift
@@ -3,11 +3,56 @@ import SwiftUI
 extension Game {
     struct Title: View {
         @Binding var session: Session
+        @State private var new = false
         @AppStorage(Defaults.Key.onboard_start.rawValue) private var start = true
         
         var body: some View {
             if start {
                 Onboard(session: $session)
+            } else if new {
+                VStack {
+                    Spacer()
+                    Text("New\nGame")
+                        .multilineTextAlignment(.center)
+                        .font(Font.largeTitle.bold())
+                        .padding(.bottom)
+                    HStack {
+                        VStack {
+                            Control.Circle(image: "person.fill") {
+                                session.match = .init()
+                            }
+                            Text("Single")
+                                .font(.caption2)
+                                .offset(y: -10)
+                                .zIndex(-1)
+                        }
+                        .frame(width: 120)
+                        VStack {
+                            Control.Circle(image: "person.2.fill") {
+                                UIApplication.shared.request()
+                            }
+                            Text("Multiplayer")
+                                .font(.caption2)
+                                .offset(y: -10)
+                                .zIndex(-1)
+                        }
+                        .frame(width: 120)
+                    }
+                    .padding(.top)
+                    Spacer()
+                    Button {
+                        withAnimation(.easeInOut(duration: 0.35)) {
+                            new = false
+                        }
+                    } label: {
+                        Text("Cancel")
+                            .foregroundColor(.secondary)
+                            .bold()
+                            .frame(minWidth: 100, minHeight: 60)
+                    }
+                    .contentShape(Rectangle())
+                    .padding(.bottom)
+                }
             } else {
                 VStack {
                     VStack {
@@ -20,7 +65,7 @@ extension Game {
                     }
                     .padding(60)
                     Spacer()
-                    Controls(session: $session)
+                    Controls(session: $session, new: $new)
                 }
             }
         }
