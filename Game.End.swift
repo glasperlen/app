f1385f5764be8a7ad6ae5afc74b2f8338e3ace1e
vaux <zero.griffin@gmail.com>
2020-12-09 14:15:50 +0100

    Update end

--- Game.End.swift
+++ Game.End.swift
@@ -10,36 +10,29 @@ extension Game {
         var body: some View {
             VStack {
                 Text("Game Over")
-                    .bold()
-                    .padding(.top)
-                if visible {
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 12)
-                            .fill(Color(.secondarySystemBackground))
-                            .frame(width: 120, height: 120)
-                        Circle()
-                            .fill(Color.black.opacity(0.2))
-                            .frame(width: 80, height: 80)
-                        Bead(bead: result.bead)
-                    }
+                    .font(Font.title.bold())
                     .padding(.top, 50)
-                    Text("This bead will be")
-                        .padding(.top)
-                    Text("removed from your inventory.")
-                    Spacer()
+                if visible {
+                    Bead(bead: result.bead)
+                    Text("This bead will be\nremoved from your inventory")
+                        .font(Font.title2.bold())
+                        .padding()
                     Control.Capsule(text: "Done", background: .primary, foreground: .black) {
+                        withAnimation(.easeInOut(duration: 0.5)) {
+                            visible = false
+                        }
                         UIApplication.shared.end()
-                        withAnimation(.easeInOut(duration: 1)) {
+                        withAnimation(.easeInOut(duration: 1.5)) {
                             session.match = nil
                             Defaults.game = nil
                         }
                     }
-                    .padding(.bottom)
+                    .padding(.vertical)
                 }
             }
             .onAppear {
                 if session[result.winner] {
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
                         session.beads.append(.init(selected: false, item: result.bead))
                         UIApplication.shared.victory()
                         UIApplication.shared.remove()
@@ -47,7 +40,7 @@ extension Game {
                         Defaults.game = nil
                     }
                 } else {
-                    session.beads.removeAll { $0.item == result.bead }
+                    session.beads.removeAll { $0.item == result.bead && $0.selected }
                     visible = true
                 }
             }
